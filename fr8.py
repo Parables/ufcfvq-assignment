3beef121cab3bc277dd4b30e85b360884ae4287c
Parables <parables95@gmail.com>
2022-01-27 23:58:36 +0000

    added fr8

--- fr8.py
+++ fr8.py
@@ -1,84 +1,104 @@
-from library import generate_stats, filename, statKey, reduce, extraSpace
+from library import generate_stats, filename, statKey, reduce, padding
 
 
-def print_stats_table(columns=[], vBorderChar="|", hBorderChar="-",seperatorChar="|"):
-    statData = generate_stats(filename);
+def print_stats_table(columns=[], divideRows=False, padding=padding, vBorderChar="|", hBorderChar="-", seperatorChar="|"):
+    statData = generate_stats(filename)
     statColumns = statData[statKey]
     del statData[statKey]
-    selectedColumns = list(statData.keys()) if columns==[] else columns
-    # print(selectedColumns)
-    # print(statData)
-    tableHeader = get_table_header(selectedColumns,vBorderChar, seperatorChar )
-    lineLength = line_length(tableHeader)
-    print_table_header(tableHeader, lineLength,hBorderChar )
-    # tableBody = get_table_rows(statColumns, statData, lineLength, vBorderChar, hBorderChar,seperatorChar)
-    # print_table_rows(tableBody)
 
-def get_table_header(columns, vBorderChar="-",seperatorChar="|"):
-    tableHeader = left_aligned("",vBorderChar, vBorderChar, 20 )
-    for (index,column) in enumerate(columns):
-        tableHeader +=  center_aligned(column, seperatorChar, ) if index <(len(columns)-1) else right_aligned(column, vBorderChar,) 
-    return tableHeader
+    selectedColumns = list(statData.keys()) if columns == [] else columns
 
-def print_table_header(tableHeader, lineLength, hBorderChar="-"):
+    cells, widths = get_header_row(["".ljust(10, " ")] +
+                             selectedColumns, padding, vBorderChar, seperatorChar)
+    lineLength = len(cells)
+    print_row(cells, lineLength, hBorderChar)
+    print_table_body(statColumns, statData, widths, padding, lineLength, vBorderChar,divideRows, hBorderChar)
+
+
+def get_header_row(cells, padding, vBorderChar="|", seperatorChar="|"):
+    rows = ""
+    widths = []
+    for (index, cell) in enumerate(cells):
+        if(index == 0):
+            c, w = left_aligned(cell, None, padding, vBorderChar, seperatorChar)
+            rows = rows + c
+            widths += [w]
+        elif(index < (len(cells)-1)):
+            c, w = center_aligned(cell, None,padding,  seperatorChar)
+            rows = rows + c
+            widths += [w]
+        else:
+            c, w = right_aligned(cell, None, padding, vBorderChar)
+            rows = rows + c
+            widths += [w]
+    return rows, widths
+
+
+def get_body_row(cells,widths, padding, vBorderChar="|", seperatorChar="|" ):
+    rows = ""
+    for (index, cell) in enumerate(cells):
+        if(index == 0):
+            c,w = left_aligned(str(cell), widths[index],padding, vBorderChar, seperatorChar)
+            rows = rows + c
+        elif(index < (len(cells)-1)):
+            c,w = center_aligned(str(cell), widths[index],padding, seperatorChar)
+            rows = rows + c
+        else:
+            c,w = right_aligned(str(cell), widths[index],padding, vBorderChar)
+            rows = rows + c
+    return rows
+
+
+def print_row(cells, lineLength, hBorderChar="-"):
     print_line(lineLength, hBorderChar)
-    print(tableHeader)
+    print(cells)
     print_line(lineLength, hBorderChar)
-    pass
 
-def categorize_data_by_stat(statColumns, statData):
-    result = {}.fromkeys(statColumns, [])
-    for (rowIndex, row) in enumerate(statColumns):
+
+def get_table_body(statColumns, statData):
+    # result = { r: [r] for (i,r) in enumerate(startColumns)}
+    result = []
+    for (firstCellIndex, firstCell) in enumerate(statColumns):
+        row = [firstCell]
         for (cellIndex, cell) in enumerate(statData):
-            result[row] = result[row] + [[cell, statData[cell][rowIndex]]]
-    # print(result)
+            row += [statData[cell][firstCellIndex]]
+        result += [row]
     return result
-    
-
-def get_table_rows(statColumns,statData,lineLength, vBorderChar="|", hBorderChar="-",seperatorChar="|" ):
-    tableRow = ""
-    rowCell = ""
-    cells = ""
-    statData = categorize_data_by_stat(statColumns, statData)
-    for (index,rowHeader) in enumerate(statColumns):
-        rowCell =  left_aligned(rowHeader,vBorderChar, vBorderChar, 20-len(rowHeader)) 
-        # print(statData[rowHeader])
-        # cells = "".join(list(map(lambda x: center_aligned(str(x[1]), seperatorChar,(len(x[0])+len(str(x[1])))-1 ), statData[rowHeader])))
-        cells = cells.join([align_c(kv[0], kv[1]) if (i<len(statData[rowHeader])-1) else align_r(kv[0], kv[1]) for (i, kv) in enumerate(statData[rowHeader])])
-        print(rowCell+cells )
-        # print_line(lineLength, hBorderChar)
-        # cellData =list(map(lambda cell: center_aligned("{}".format(cell), seperatorChar, 20-len("{}".format(cell)))  , statData[rowHeader]))
-        # tableRow.join(cellData)
-    return tableRow
-
-align_c= lambda k,v, seperatorChar="|": center_aligned(str(v), seperatorChar,(len(k)+len(str(v)))-1 )
-align_r= lambda k,v, vBorderChar="|": right_aligned(str(v),vBorderChar, (len(k)+len(str(v)))-1 )
-
-def print_table_rows(tableBody):
-    print(tableBody)
-
-def  left_aligned(string,vBorderChar="|", seperatorChar="|",   extraSpace=extraSpace, fillchar=" "):
-    return vBorderChar + " " + string.ljust(len(string)+extraSpace, fillchar ) + seperatorChar
-
-def  right_aligned(string, vBorderChar="-",   extraSpace=extraSpace, fillchar=" "):
-    return string.rjust(len(string)+extraSpace, fillchar ) + " " + vBorderChar 
 
 
-def  center_aligned(string,seperatorChar="|",   extraSpace=extraSpace, fillchar=" "):
-    # print("len= {0}, extraSpace={1}, total={2} ".format(len(string),extraSpace, len(string)+extraSpace))
-    return   string.center(len(string)+extraSpace, fillchar ) + seperatorChar
+def print_table_body(statColumns, statData, widths, padding, lineLength,vBorderChar, divideRows=False, hBorderChar="-"):
+    rows = get_table_body(statColumns, statData)
+    for cells in rows:
+        row = get_body_row(cells, widths, padding)
+        print(row)
+        if(divideRows): print_line(lineLength, "_")
+    print_line(lineLength)
 
 
-sum_of_column_widths= lambda length, column: length + len(column)+extraSpace
+def left_aligned(string, width=None, padding=padding, vBorderChar="|", seperatorChar="|", fillchar=" "):
+    cell = vBorderChar + " " + \
+        string.ljust(
+            len(string)+padding if width is None else width, fillchar) + seperatorChar
+    width = len(cell) - (len(vBorderChar) + len(seperatorChar))
+    return cell, width
 
-def line_length(tableHeader):
-   return len(tableHeader)
+
+def right_aligned(string, width=None, padding=padding, vBorderChar="|", fillchar=" "):
+    cell = string.rjust(
+        len(string)+padding if width is None else width, fillchar) + " " + vBorderChar
+    width = len(cell) - len(vBorderChar)
+    return cell, width
+
+
+def center_aligned(string, width=None, padding=padding, seperatorChar="|", fillchar=" "):
+    cell = string.center(
+        len(string)+padding if width is None else width, fillchar) + seperatorChar
+    width = len(cell) - len(seperatorChar)
+    return cell, width
+
 
 def print_line(lineLength, lineChar="-"):
     return print(lineChar * lineLength)
 
 
-
-
-
-print_stats_table();
+print_stats_table()
